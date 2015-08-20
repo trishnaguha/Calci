d44c5fd766416b3370a8922d414a69b898804886
trishnaguha <trishnaguha17@gmail.com>
2015-08-21 00:15:31 +0530

    Adding more features and some fixes

--- calci.py
+++ calci.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
+import math
 
 "Performs addition"
 def add(a,b):
@@ -22,18 +23,30 @@ def multi(a,b):
 	val = a*b
 	return val
 
+"Performs percentage"
+def perc(a):
+	val = a/100
+	return val
+
+"Performs factorial"
+def fact(a):
+	return math.factorial(a)
+
 "The Main function"
 def main():
-	parser = argparse.ArgumentParser(description = "A basic Calculator")
+	parser = argparse.ArgumentParser(description = "A Command Line Calculator", epilog = "Enter two numbers for addition,subtraction and so on.... \n Enter only one number for percentage calculation,factorial and so on....")
 	
 	group = parser.add_mutually_exclusive_group()
-	group.add_argument("-fa","--fadd",help = "Performs addition",action = "store_true")
-	group.add_argument("-fs","--fsub",help = "Performs subtraction",action = "store_true")
-	group.add_argument("-fd","--fdiv",help = "Performs division",action = "store_true")
-	group.add_argument("-fm","--fmulti",help = "Performs multiplication",action = "store_true")
+	group.add_argument("-fa", "--fadd", help = "Performs addition", action = "store_true")
+	group.add_argument("-fs", "--fsub", help = "Performs subtraction", action = "store_true")
+	group.add_argument("-fd", "--fdiv", help = "Performs division", action = "store_true")
+	group.add_argument("-fm", "--fmulti", help = "Performs multiplication", action = "store_true")
+	group.add_argument("-fp", "--fperc", help = "Performs percentage calculation", action = "store_true")
+	group.add_argument("-ff", "--ffact", help = "Performs factorial calculation", action = "store_true")
+	
 
-	parser.add_argument("num1",help = "Number1 to calculate",type = int)
-	parser.add_argument("num2",help = "Number2 to calculate",type = int)
+	parser.add_argument("num1", help = "Number1 to calculate", type = int)
+	parser.add_argument("num2", nargs='?', help = "Number2 to calculate, this is optional", type = int)
 
 	args = parser.parse_args()
 
@@ -45,6 +58,10 @@ def main():
 		print("The division result of {} and {} is {}".format(args.num1,args.num2,(div(args.num1,args.num2))))
 	elif args.fmulti:
 		print("The multiplication result of {} and {} is {}".format(args.num1,args.num2,(multi(args.num1,args.num2))))
+	elif args.fperc:
+		print("The percentage of {} is {}".format(args.num1,(perc(args.num1))))
+	elif args.ffact:
+		print("The factorial of {} is {}".format(args.num1,(fact(args.num1))))
 	else:
 		print("Error: requires an argument to perform an action\nType python3 calci.py -h or python3 calci.py --help for help")
 	
